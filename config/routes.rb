816b9e625d02a7bb3141e0380541f068dc2041db
tatina555 <katekinex100@gmail.com>
2020-11-16 14:51:34 +0900

    トップページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'furima/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-def index
-end
+  root  to: 'furima#index'
 
 end
