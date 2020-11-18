439aff8ff8ee8da9680659eaf49b614ca967a17c
tatina555 <katekinex100@gmail.com>
2020-11-18 18:20:52 +0900

    テストコード記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :birthday, presence: true
+
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
