18a09f6cd28ae0a75a6cac31dc36006ca8805344
tatina555 <katekinex100@gmail.com>
2020-11-19 00:10:50 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
 end
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,19 @@
+class ItemsController < ApplicationController
+
+  def index 
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:items).permit(:content, :image).merge(user_id: current_user.id)
+  end
+end
