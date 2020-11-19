e11b2e57e5a7bbe3d2b995de5d17b8ddc8c2f752
tatina555 <katekinex100@gmail.com>
2020-11-19 11:31:15 +0900

    アクティブハッシュ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :details_category_id,     presence: true
+  validates :details_status_id,       presence: true
+  validates :delivery_burden_id,      presence: true
+  validates :delivery_source_id,      presence: true
+  validates :delivery_day_id,         presence: true
+
+  validates :genre_id, numericality: { other_than: 1 } 
 end
