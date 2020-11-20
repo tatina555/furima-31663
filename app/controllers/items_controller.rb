c9e88022629e91db3357ceda3f8a6390bce9e7c2
tatina555 <katekinex100@gmail.com>
2020-11-20 18:56:36 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :create, :show]
+  before_action :edit_to_index, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -23,6 +24,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def move_to_index
@@ -34,5 +48,12 @@ class ItemsController < ApplicationController
       :image, :title, :catch_copy, :details_category_id, :details_status_id,
       :delivery_burden_id, :delivery_source_id, :delivery_day_id, :price
     ).merge(user_id: current_user.id)
+  end
+
+  def edit_to_index
+    item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == item.user.id
+      redirect_to root_path
+    end
   end
 end
