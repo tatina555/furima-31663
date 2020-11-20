92f3b88b0287886d24c06368477de3a8d01af470
tatina555 <katekinex100@gmail.com>
2020-11-20 19:28:15 +0900

    商品編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :create, :show]
+  before_action :item_find, only:[:show, :edit, :update, :edit_to_index]
   before_action :edit_to_index, only: [:edit]
+
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -21,15 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -50,8 +49,11 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
   def edit_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless user_signed_in? && current_user.id == item.user.id
+    redirect_to root_path unless current_user.id == item.user.id
   end
 end
