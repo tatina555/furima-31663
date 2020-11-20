ab9552373ed1e69f6c162f6392f005971cdb19ed
tatina555 <katekinex100@gmail.com>
2020-11-20 16:14:22 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  before_action :move_to_index, except: [:index, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :create, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
