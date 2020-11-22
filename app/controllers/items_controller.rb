1b05ce0c43b4dc545496dfd86f5a5cf41b7dd42f
tatina555 <katekinex100@gmail.com>
2020-11-22 15:23:37 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :create, :show]
   before_action :item_find, only:[:show, :edit, :update, :destroy, :edit_to_index]
-  before_action :edit_to_index, only: [:edit]
+  before_action :edit_to_index, only: [:edit, :destroy]
 
 
   def index
